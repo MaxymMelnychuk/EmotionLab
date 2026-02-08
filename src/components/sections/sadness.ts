c408f09655455fc0f236467bfc95b426f6982346
MaxymMelnychuk <maxymmelnychuk100@gmail.com>
2026-02-08 14:56:21 +0100

    feat: add Fear section with screamer image and stacking animation

--- src/components/sections/sadness.ts
+++ src/components/sections/sadness.ts
@@ -1,6 +1,13 @@
+import rainEffect from "../rain";
+
 export default function Sadness(): HTMLElement {
     const div = document.createElement('div');
     div.classList.add("sadness", "container");
-    div.innerHTML = `<p>Sadness</p>`;
+    div.innerHTML = `<p class="emotion_title">Sadness</p>`;
+
+    setInterval(() => {
+        rainEffect(div)
+    }, 1)
+    
     return div;
 }
